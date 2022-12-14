9d10a7c68853a3cda08bcb9d47f9fc1f3523b9e5
isma-91 <isma91@hotmail.it>
2022-12-14 16:25:02 +0100

    Add snack 2

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,3 +1,25 @@
+<?php 
+/*
+## Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+ */
+?>
+
+<?php 
+$name = isset($_GET['name']) ? $_GET['name'] : '';
+$mail = isset($_GET['mail']) ? $_GET['mail'] : '';
+$age = isset($_GET['age']) ? $_GET['age'] : '';
+
+$pos_at = strpos($mail, '@');
+$pos_dot = strpos($mail, '.', $pos_at);
+
+		if (strlen(trim($name)) > 3 && $pos_at && $pos_dot && is_numeric($age)) {
+			$message = 'Accesso riuscito';
+		} else {
+			$message = 'Accesso negato';
+		}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +29,20 @@
   <title>Document</title>
 </head>
 <body>
+  <form action="" method="get">
+		<label for="name">Nome da controllare</label>
+		<input type="text" name="name" id="name">
+
+		<label for="mail">Email da controllare</label>
+		<input type="mail" name="mail" id="mail">
+
+		<label for="age">Età da controllare</label>
+		<input type="text" name="age" id="age">
+
+		<button>Controlla</button>
+	</form>
+
+  <h2><?= $message ?></h2>
 
 </body>
 </html>
